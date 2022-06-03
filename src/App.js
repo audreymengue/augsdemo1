83cb64fd2e337f89ede28c9899cd7c75092aa303
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-03 23:55:08 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
+       
+      </section>
+    </main>
+  )
+}
+
+export default App
