78283b276e7cc2ff5653f9ae955c84bd2e0b5755
Audrey Mengue <massoumeharmonie@gmail.com>
2022-06-04 01:06:24 +0000

    Updated gh-pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     <main>
       <section className='container'>
         <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
+        <h3>Hey, CI/CD here with Azure devOps pipelines, we tried and it worked perfectly.</h3>
        
       </section>
     </main>
